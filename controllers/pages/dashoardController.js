e18cf38677b41f12a5e8f29cd3a0f80dedb30108
Vivek Mehra <vivek.mehra@squareyards.co.in>
2025-04-09 15:57:33 +0530

    changes added

--- controllers/pages/dashoardController.js
+++ controllers/pages/dashoardController.js
@@ -15,22 +15,21 @@ const getDashboardPage = async (req,res) => {
             supplier_count: [],
         }
         const facilityRes = await facilityService.getLatestFacility(5);
-        console.log('facilityRes === > ', facilityRes);
-        // if(facilityRes && facilityRes.data) {
-        //     model.facility_data = facilityRes.data;
-        // }
-        // const facilityCountRes = await facilityService.getFacilityCount();
-        // if(facilityCountRes && facilityCountRes.data) {
-        //     model.facility_count = facilityCountRes.data;
-        // }
-        // const supplierRes = await supplierServices.getLatestSupplier(5);
-        // if(supplierRes && supplierRes.data) {
-        //     model.supplier_data = supplierRes.data;
-        // }
-        // const supplierCountRes = await supplierServices.getSupplierCount();
-        // if(supplierCountRes && supplierCountRes.data) {
-        //     model.supplier_count = supplierCountRes.data;
-        // }
+        if(facilityRes && facilityRes.data) {
+            model.facility_data = facilityRes.data;
+        }
+        const facilityCountRes = await facilityService.getFacilityCount();
+        if(facilityCountRes && facilityCountRes.data) {
+            model.facility_count = facilityCountRes.data;
+        }
+        const supplierRes = await supplierServices.getLatestSupplier(5);
+        if(supplierRes && supplierRes.data) {
+            model.supplier_data = supplierRes.data;
+        }
+        const supplierCountRes = await supplierServices.getSupplierCount();
+        if(supplierCountRes && supplierCountRes.data) {
+            model.supplier_count = supplierCountRes.data;
+        }
         model.title = 'Dashboard | Tracibility';
         model.layout = 'layouts/dashboard-layout';
         console.timeEnd('dashboardPage === > ');
