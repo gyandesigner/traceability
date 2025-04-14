f5a946cfbe83cec36b6b5cc0fed535bb0ee7cbd6
Vivek Mehra <vivek.mehra@squareyards.co.in>
2025-04-14 11:22:02 +0530

    athenticiate user

--- routes/api/facilityMaster.js
+++ routes/api/facilityMaster.js
@@ -6,7 +6,7 @@ import authenticateToken from '../../middleware/authMiddleware.js';
 router.get('/get-all-facility', facilityController.getAllFacility);
 router.get('/get-recent-facility/:length', facilityController.getRecentFacility);
 router.get('/get-facility-count', facilityController.getFacilityCount);
-router.post('/add-facility', facilityController.addFacility);
+router.post('/add-facility', authenticateToken, facilityController.addFacility);
 router.post('/update-facility/:id', facilityController.updateFacilityById);
 router.get('/delete-facility/:id', facilityController.deleteFacilityById);
 
